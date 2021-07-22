eced79cee3b8c28e17e255aa8a335625f7eb8a9e
SAHASRABDI <sahasrabdi721306@gmail.com>
2021-07-22 15:19:58 +0530

    doubly linked list deletion

--- LinkedList/doublyLinkedList/DoublyLL.java
+++ LinkedList/doublyLinkedList/DoublyLL.java
@@ -94,6 +94,91 @@ class DoublyLL {
 
     }
 
+    // deletion function
+    public Node deleteNode(int delVal) {
+        // we can also not return head cause head is a class variable and it changes for
+        // entire class
+        // if we had used static variables and declared the head in the main method we
+        // had to return the head
+        // if no node
+        if (head == null) {
+            System.out.println("No node to delete");
+            return (head);
+        } else {
+            // the temp and prev
+            Node temp = head;
+            Node previous = null;
+            // if head node has to be deleted and there is only the head node
+            if (temp.next == null && temp.data == delVal) {
+                head = null;
+                return (head);
+            }
+
+            // head node to be deleted and but not the on;ly node
+            if (temp.next != null && temp.data == delVal) {
+                head = temp.next;
+                temp.next.prev = null;
+                return (head);
+            }
+            // traverse until found
+            while (temp != null && temp.data != delVal) {
+                previous = temp;
+                temp = temp.next;
+            }
+            // if value not present then we reach temp==null
+            if (temp == null) {
+                System.out.println("Value Not found");
+                return (head);
+            }
+            previous.next = temp.next;
+            // if last node then temp.next.prevoius wont be there
+            if (temp.next == null) {
+                return head;
+            }
+            temp.next.prev = previous;
+            return (head);
+
+        }
+    }
+
+    public void deleteAtPos(int pos) {
+        int size = calcSize(head);
+
+        if (pos < 1 || pos > size) {
+            System.out.println("Invalid position");
+
+        } else {
+            Node temp = head;
+            // Node previous=null;
+            // if only 1 node
+            if (pos == 1 && temp.next == null) {
+                head = null;
+                System.out.println("Value at position 1 which is " + temp.data + " is deleted");
+                return;
+
+            }
+            if (pos == 1 && temp.next != null) {
+                head = temp.next;
+                temp.next.prev = null;
+                System.out.println("Value at position 1 which is " + temp.data + " is deleted");
+                return;
+            }
+
+            // traverse until found
+            for (int i = 0; i < pos - 2; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next.next == null) {
+                System.out.println("Deleted");
+                return;
+            }
+            temp.next.next.prev = temp;
+            System.out.println("Value deleted");
+
+        }
+    }
+
     public void display() {
         Node end = null;
         Node node = head;
@@ -118,14 +203,22 @@ class DoublyLL {
     public static void main(String args[]) {
         DoublyLL list = new DoublyLL();
         // list.insertEnd(2);// no node first
+        list.deleteNode(50);// not present starting node
         list.insertStart(20);
         // list.display();
+        list.deleteNode(20);// delete head node ony 1 node
         list.insertStart(30);
         list.insertStart(40);
         list.insertEnd(50);
+        list.deleteNode(40);// delete headNode but others present
         list.insertPos(1, 3);
         list.insertPos(5, 100);
+        list.deleteNode(3);// delete random node
+        list.deleteNode(50);// last node
+        list.deleteNode(90);// delete node not present
+        // 30
         list.display();
+        list.deleteAtPos(1);
 
     }
 
