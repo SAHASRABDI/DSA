52da6f0632b7f03475c1f2c37f1bc8590225ec08
SAHASRABDI <sahasrabdi721306@gmail.com>
2021-07-23 21:51:45 +0530

    circularLL deletion

--- LinkedList/circularLinkedList/CircularLL.java
+++ LinkedList/circularLinkedList/CircularLL.java
@@ -14,6 +14,22 @@ class Node {
 public class CircularLL {
     Node head = null;
 
+    public int calcSize() {
+
+        if (head.next == head) {
+            return (1);
+        } else {
+            int size = 0;
+            Node temp = head.next;
+            while (temp != head) {
+                temp = temp.next;
+                size++;
+            }
+            System.out.println(size + 1);
+            return (size + 1);
+        }
+    }
+
     public void insertStart(int data) {
         Node newNode = new Node(data);
         // if this is the first node
@@ -50,6 +66,44 @@ public class CircularLL {
         }
     }
 
+    public void deleteStart() {
+        if (head == null) {
+            System.out.println("No node to delete");
+            return;
+        }
+        if (head.next == head) {
+            head = null;
+
+        } else {
+            head.data = head.next.data;
+            head.next = head.next.next;
+
+        }
+
+    }
+
+    public void deleteAtPos(int pos) {
+        if (head == null) {
+            System.out.println("No node to delete");
+            return;
+        }
+        int size = calcSize();
+        if (pos < 1 || pos > size) {
+            System.out.println("Invalid position");
+            return;
+        }
+        if (pos == 1) {
+            deleteStart();
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < pos - 2; i++) {
+            current = current.next;
+        }
+        current.next = current.next.next;
+
+    }
+
     void display() {
         if (head == null) {
             return;
@@ -60,17 +114,27 @@ public class CircularLL {
             System.out.print(temp.data + " ");
             temp = temp.next;
         } while (temp != head);
+        System.out.println();
 
     }
 
     public static void main(String args[]) {
         // create an object
         CircularLL list = new CircularLL();
+        list.deleteAtPos(2);
         list.insertLast(0);
+        list.display();
+        list.deleteStart();
         list.insertStart(10);
+        list.display();
         list.insertStart(20);
         list.insertStart(30);
         list.insertLast(100);
+        list.display();
+        list.deleteAtPos(4);
+        list.display();
+        list.deleteStart();
+        list.deleteAtPos(20);
         list.display();
 
     }
